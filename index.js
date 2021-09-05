13bef1eb94481dc260e1816e6d9657f719430039
Sainath <sainathsvm@gmail.com>
2021-09-06 01:50:24 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+function reverseStr(str){
+    return str.split('').reverse().join('');
+}
+
+function isPalindrome(str){
+    var reverse = reverseStr(str);
+
+    return str === reverse;
+}
+
+function convertDateToStr(date) {
+    var dateStr = { day: "", month: "", year: "" };
+  
+    if (date.day < 10) {
+      dateStr.day = "0" + date.day;
+    } else {
+      dateStr.day = date.day.toString();
+    }
+  
+    if (date.month < 10) {
+      dateStr.month = "0" + date.month;
+    } else {
+      dateStr.month = date.month.toString();
+    }
+  
+    dateStr.year = date.year.toString();
+  
+    return dateStr;
+  }
+
+  function getAllDateFormats(date){
+      var dateStr = convertDateToStr(date);
+    
+      var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+      var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+      var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+      var ddmmyyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+      var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+      var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+
+      return [ddmmyyyy,mmddyyyy,yyyymmdd,ddmmyyy,mmddyy,yymmdd]
+  }
